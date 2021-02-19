0505a8fac74f68f210fea7e19f47fc8ee5294d7d
ALEXIS GUETAT <alexis.guetat34@hotmail.fr>
2021-02-19 11:58:46 +0100

    maj maps

--- src/app/chemin/chemin.component.ts
+++ src/app/chemin/chemin.component.ts
@@ -20,89 +20,41 @@ ngOnInit(): void {
 
 ngAfterViewInit(){
   this.initMap();
-
-  
 }
 
 
 initMap(): void {
   const directionsService = new google.maps.DirectionsService();
   const directionsRenderer = new google.maps.DirectionsRenderer();
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 7,
+      center: { lat: 41.85, lng: -87.65 },
+    }
+  );
+  directionsRenderer.setMap(map);
 
-  var propriete_maps = {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+  const onChangeHandler = function () {
+    directionsService.route(
+      {
+        origin: {
+          query: "EPSI, Montpellier, France",
+        },
+        destination: {
+          query: "Gare, Montpellier, France",
+        },
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (response, status) => {
+        if (status === "OK") {
+          directionsRenderer.setDirections(response);
+        } else {
+          window.alert("Directions request failed due to " + status);
+        }
+      }
+    );
   };
-
-  this.map = new google.maps.Map(document.getElementById("map"), propriete_maps);
-    // Try HTML5 geolocation.
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-        this.map.setCenter(pos);
-        const marker = new google.maps.Marker({
-          position: pos,
-          map: this.map,
-          icon: {
-            url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-          }
-        });
-});
-const onChangeHandler = function () {
-  directionsService.route(
-    {
-      origin: {
-        query: (document.getElementById("start") as HTMLInputElement).value,
-      },
-      destination: {
-        query: (document.getElementById("end") as HTMLInputElement).value,
-      },
-      travelMode: google.maps.TravelMode.DRIVING,
-    },
-    (response, status) => {
-      if (status === "OK") {
-        directionsRenderer.setDirections(response);
-      } else {
-        window.alert("Directions request failed due to " + status);
-      }
-    }
-  );
-};
-(document.getElementById("start") as HTMLElement).addEventListener(
-  "change",
-  onChangeHandler
-);
-(document.getElementById("end") as HTMLElement).addEventListener(
-  "change",
-  onChangeHandler
-);
-}
-
-calculateAndDisplayRoute(
-  directionsService: google.maps.DirectionsService,
-  directionsRenderer: google.maps.DirectionsRenderer
-) {
-  directionsService.route(
-    {
-      origin: {
-        query: (document.getElementById("start") as HTMLInputElement).value,
-      },
-      destination: {
-        query: (document.getElementById("end") as HTMLInputElement).value,
-      },
-      travelMode: google.maps.TravelMode.DRIVING,
-    },
-    (response, status) => {
-      if (status === "OK") {
-        directionsRenderer.setDirections(response);
-      } else {
-        window.alert("Directions request failed due to " + status);
-      }
-    }
-  );
+  onChangeHandler();
 }
 }
